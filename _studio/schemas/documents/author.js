27fe7ff6f466421d7d992b602ca5a34ad551a28b
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-04-26 16:08:18 +0200

    Created sanity schemas (#10)

---
+++ _studio/schemas/documents/author.js
@@ -0,0 +1,17 @@
+export default {
+	title: 'Author',
+	name: 'author',
+	type: 'document',
+	fields: [
+		{
+			title: 'Name',
+			name: 'name',
+			type: 'string',
+		},
+		{
+			title: 'Image',
+			name: 'image',
+			type: 'image',
+		}
+	]
+}
