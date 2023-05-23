79f8a93647589d7582e5abf5874b6fd0d744f759
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-05-23 13:54:18 +0200

    Added loader and clear local storage

--- _app/js/modules/portfolio.js
+++ _app/js/modules/portfolio.js
@@ -1,7 +1,57 @@
 import fetchStockAPI from "./fetchStockAPI.js";
+import stockDialog from "./stockDialog.js";
 
+const portfolioGainRow = document.querySelector('.portfolio__total-gain-row');
+const localData = localStorage.getItem('stockList');
+
+let spinner = true;
 export default function portfolio() {
-	fetchStockAPI()
+	displayLoading()
+	console.log(spinner)
+	
+	
+	fetchStockAPI();
+	stockDialog();
 }
 
-portfolio()
+const portfolioSection = document.querySelector('.portfolio');
+const portfolioSpinner = document.createElement('img');
+
+function displayLoading() {
+	portfolioSpinner.className = 'portfolio__spinner'
+	
+	portfolioSpinner.src = "../_app/assets/icons/loader.svg"
+	
+	portfolioSection.append(portfolioSpinner)
+}
+
+function hideLoading() {
+	portfolioSpinner.remove()
+}
+
+export {hideLoading}
+
+if (localData) {
+	const clearLocalStorageButton = document.createElement('button');
+
+	clearLocalStorageButton.addEventListener('click', handleClearLocalStorage)
+
+	clearLocalStorageButton.innerText = 'Clear all';
+	clearLocalStorageButton.className = 'primary-button remove-button';
+
+	portfolioGainRow.append(clearLocalStorageButton);
+
+	function handleClearLocalStorage() {
+		localStorage.clear()
+		console.log('clear localstorage')
+		location.reload()
+	}
+} else {
+	const noLocalStorageText = document.createElement('h2');
+
+	noLocalStorageText.innerText = `Click 'Add stock' to start your journey`;
+
+	noLocalStorageText.className = 'add-stock-reminder__text h2';
+
+	portfolioSection.append(noLocalStorageText)
+}
