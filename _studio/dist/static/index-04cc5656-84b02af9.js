59c1005d5041ad8acd3712399f928a517872ad72
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-05-23 14:30:18 +0200

    Deployed Sanity studio

---
+++ _studio/dist/static/index-04cc5656-84b02af9.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-b167003e-c21d24c9.js";import"./sanity-e7885e40.js";import"./index-18ebfb3e.js";export{m as DocumentPaneProvider,a as default};
